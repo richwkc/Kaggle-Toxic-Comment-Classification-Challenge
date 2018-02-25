6673cc8ef5eff25810be65e9728a660f091d555e
mariosk <marios.koulakis@gmail.com>
2018-02-25 06:46:48 -0500

    Moves tensorboard logs only if existing and samples test set instead of chuncking it.

--- keras_train_helper.py
+++ keras_train_helper.py
@@ -5,6 +5,8 @@ import re
 import shutil
 import keras.backend as K
 import tensorflow as tf
+import numpy as np
+import os.path 
 
 def tfauc(y_true, y_pred):
      auc = tf.metrics.auc(y_true, y_pred)[1]
@@ -12,30 +14,39 @@ def tfauc(y_true, y_pred):
      return auc
 
 def rotateTensorboardLogs():
-    copyEnding = re.sub(r"[\s\:\.]", "-", str(datetime.datetime.now()))
-    shutil.move("./tb-logs", "./tb-logs-{}".format(copyEnding))
+    if os.path.isdir("./tb-logs"):
+        copyEnding = re.sub(r"[\s\:\.]", "-", str(datetime.datetime.now()))
+        shutil.move("./tb-logs", "./tb-logs-{}".format(copyEnding))
 
 class PrintAucCallback(Callback):
     def __init__(self, sentences, labels, printPerBatches=None):
+        np.random.seed(64563)
+        tenthOfCount = int(sentences.shape[0] / 10)
+        
         self.testData = [sentences, labels]
-        self.smallTestData = [sentences[:int(sentences.shape[0] / 10)], labels[:int(labels.shape[0] / 10)]]
+        self.smallTestData = [
+            sentences[np.random.choice(sentences.shape[0], tenthOfCount, replace=False)], 
+            labels[np.random.choice(labels.shape[0], tenthOfCount, replace=False)] ]
         self.printPerBatches = printPerBatches
         self.listOfAucs = []
 
-    def computeAuc(self, small=False):
-        sentences, labels = self.smallTestData if small else self.testData
+    def computeAuc(self, smallTestSet=False):
+        sentences, labels = self.smallTestData if smallTestSet else self.testData
         predictions = self.model.predict(sentences)
         return roc_auc_score(labels[:, 1], predictions[:, 1])
         
-    def printAuc(self, auc):
-        print(" - auc: {:.4f}".format(auc))
+    def printAuc(self, auc, smallTestSet=False):
+        largeTestSet = "" if smallTestSet else "all-test-data-"
+        print(" - {}auc: {:.4f}".format(largeTestSet, auc))
         
     def on_batch_end(self, batch, logs={}):
         if self.printPerBatches and (batch + 1) % self.printPerBatches == 0:
             self.printAuc(self.computeAuc())
     
     def on_epoch_end(self, epoch, logs={}):
-        auc = self.computeAuc() if (epoch + 1) % 5 == 0 else self.computeAuc(small=True)
-        self.printAuc(auc)
+        smallTestSet = (epoch + 1) % 5 != 0
+        auc = self.computeAuc(smallTestSet=smallTestSet)
+        
+        self.printAuc(auc, smallTestSet=smallTestSet)
         self.listOfAucs.append((epoch + 1, auc))
         
